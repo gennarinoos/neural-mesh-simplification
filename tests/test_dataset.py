43ee8d2e2345fd7db39f49d2d3359802dd94e105
Gennaro <gennarinoos@gmail.com>
2025-02-04 16:17:39 -0800

    fix: start fixing unit tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,9 +1,13 @@
+import dgl
 import numpy as np
+import torch
 import trimesh
+from numpy.testing import assert_array_equal
 
 from neural_mesh_simplification.data.dataset import (
     preprocess_mesh,
-    load_mesh,
+    load_mesh, mesh_to_dgl,
+    dgl_to_trimesh, collate
 )
 
 
@@ -34,3 +38,35 @@ def test_preprocess_mesh_scaled(sample_mesh):
         processed_mesh.vertices.max(axis=0) - processed_mesh.vertices.min(axis=0)
     )
     assert np.isclose(max_dim, 1.0), "Mesh is not scaled to unit cube"
+
+
+def test_face_serde(sample_mesh):
+    orig_faces = torch.tensor(sample_mesh.faces, dtype=torch.int64)
+    g, faces = mesh_to_dgl(sample_mesh)
+
+    assert torch.equal(orig_faces, faces)
+
+    r_mesh = dgl_to_trimesh(g, faces)
+
+    assert_array_equal(r_mesh.vertices, sample_mesh.vertices)
+    assert_array_equal(r_mesh.faces, sample_mesh.faces)
+
+
+def test_padding():
+    g1 = dgl.graph(([0, 1], [1, 2]), num_nodes=3)
+    g2 = dgl.graph(([0, 1, 2], [1, 2, 0]), num_nodes=3)
+    f1 = torch.tensor([[0, 1, 2], [1, 2, 0]])
+    f2 = torch.tensor([[0, 1, 2], [1, 2, 0], [2, 0, 1]])
+
+    batch = [(g1, f1), (g2, f2)]
+
+    # Pad
+    _, padded_faces = collate(batch)
+
+    # Unpad
+    unpadded_faces = [f[~(f == -1).all(dim=1)] for f in padded_faces]
+
+    # Assert idempotency
+    for original, unpadded in zip([f1, f2], unpadded_faces):
+        assert torch.all(original == unpadded)
+        assert original.shape == unpadded.shape
